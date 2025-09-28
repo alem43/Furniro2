0b6dc7abca681e4cd9e2bb6fb7ea65b587ebe283
Alem Begunic <begunicalem6@gmail.com>
2025-09-28 14:29:31 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import "./styles/home.scss";
 import "./styles/shop.scss";
+import "./styles/style.scss";
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("site-loader");
+  if (!loader) return;
+  setTimeout(() => {
+    loader.classList.add("hidden");
+    setTimeout(() => loader.remove(), 400);
+  }, 200);
+});
